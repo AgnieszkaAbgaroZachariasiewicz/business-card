e0656f9d79476c80b28445f96f3d91181ba4c80a
AgnieszkaAbgaroZachariasiewicz <agnieszka.zachariasiewicz@infakt.pl>
2021-11-28 19:54:32 +0100

    fixes

--- Main.java
+++ Main.java
@@ -3,34 +3,33 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("Podaj imie:");
 		String name = scanner.next();
-		
+
 		System.out.println("Podaj nazwisko:");
 		String surname = scanner.next();
-		
+
 		System.out.println("Podaj telefon:");
 		String tel = scanner.next();
-		
+
 		System.out.println("Podaj miasto:");
 		String city = scanner.next();
-		
+
 		String firstLine = "* " + name + " " + surname + " ";
 		String lastLine = "* tel. " + tel + " adres: " + city + " ";
 		int frameLength = lastLine.length() > firstLine.length() ? lastLine.length() + 1 : firstLine.length() + 1;
-		
+
 		StringBuilder frame = new StringBuilder();
 		for (int i = 0; i < frameLength; i++) {
 			frame.append("*");
-		} 
-		
+		}
+
 		String rigthFrame = "*";
-		
 		int fnumber;
 		int snumber;
-		
-		if ( lastLine.length() > firstLine.length()) {
+
+		if (lastLine.length() > firstLine.length()) {
 			fnumber = lastLine.length() - firstLine.length();
 			snumber = 0;
 		}  else if (firstLine.length() > lastLine.length()) {
@@ -40,17 +39,17 @@ public class Main {
 			fnumber = 0;
 			snumber = 0;
 		}
-		
+
 		StringBuilder xf = new StringBuilder();
 		for (int i = 0; i < fnumber; i++) {
 			xf.append(" ");
 		}
-		
+
 		StringBuilder xs = new StringBuilder();
 		for (int i = 0; i < snumber; i++) {
 			xs.append(" ");
 		}
-		
+
 		System.out.println(frame);
 		System.out.println(firstLine + xf + rigthFrame);
 		System.out.println(lastLine + xs + rigthFrame);
